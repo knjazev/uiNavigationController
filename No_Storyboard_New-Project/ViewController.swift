a84661087e60623d1c76d4e48e45fc7327c748c7
Dmitry Kniazev <knjazev@me.com>
2020-11-25 13:58:50 +0300

    Initial Commit

---
+++ No_Storyboard_New-Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  No_Storyboard_New-Project
+//
+//  Created by Князев Дмитрий on 11/25/20.
+//  Copyright © 2020 Dmitry Kniazev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
